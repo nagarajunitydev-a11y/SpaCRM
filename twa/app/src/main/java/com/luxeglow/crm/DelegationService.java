1e3c596e511e7c691e3cc0722c61a0ed893e3013
SalonCRMApp <saloncrm@internal.test>
2026-08-14 19:58:02 +0530

    build: add TWA Android app (com.luxeglow.crm) and Digital Asset Links for Google sign-in

---
+++ twa/app/src/main/java/com/luxeglow/crm/DelegationService.java
@@ -0,0 +1,14 @@
+package com.luxeglow.crm;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
